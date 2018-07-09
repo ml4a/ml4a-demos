5a7f7e13144a980931755606d250e2e0ac3a2497
Gene Kogan <kogan.gene@gmail.com>
2018-07-09 13:56:11 +0200

    css file

--- examples/classification-simple/sketch.js
+++ examples/classification-simple/sketch.js
@@ -1,31 +1,18 @@
 // how many classes
-let numClasses = 6;
+let numClasses = 3;
 
 let featureExtractor;
 let classifier;
 let video;
 let loss;
-let img;
 let numImages;
-let samples;
 let label;
 let buttons;
 
 function setup() {
-  createCanvas(500, 500);
-
-  img = loadImage("./assets/guitar.png");
-
-  samples = [];
-  samples[0] = loadSound('./assets/C.mp3')
-  samples[1] = loadSound('./assets/F.mp3')
-  samples[2] = loadSound('./assets/G.mp3')
-  samples[3] = loadSound('./assets/Am.mp3')
-  samples[4] = loadSound('./assets/Dm.mp3')
-  samples[5] = loadSound('./assets/Em.mp3')
+  noCanvas();
 
   label = 0;
-
   numImages = Array(numClasses).fill(0);
 
   video = createCapture(VIDEO);
@@ -36,7 +23,6 @@ function setup() {
   classifier = featureExtractor.classification(video);
   
   createButtons();
-
 }
 
 function modelReady() {
@@ -87,14 +73,5 @@ function createButtons() {
 
 function gotResults(nextLabel) {
   select('#result').html(nextLabel);
-  if (label != nextLabel) {
-    label = nextLabel;
-    samples[int(label)].play();  
-  }
-  classify();
-}
-
-function draw() {
-  background(0);
-  image(img, 50, 50);
+  label = nextLabel;
 }
